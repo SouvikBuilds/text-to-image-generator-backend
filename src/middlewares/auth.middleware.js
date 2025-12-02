a3097507fba72c3cd5efff6e8c0bd738346ed32c
Souvik Chatterjee <souvikchatterjee@Souviks-MacBook-Air.local>
2025-12-02 16:08:37 +0530

    Backend middleware updated

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
-      req.header("Authorization").replace("Bearer ", "");
+      req.header("Authorization")?.replace("Bearer ", "");
     const decodedToken = jwt.verify(token, config.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
