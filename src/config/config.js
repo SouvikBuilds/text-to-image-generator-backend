90a9952d5f0d49152f0012bf18ead1c467911a5d
Souvik Chatterjee <souvikchatterjee@Souviks-MacBook-Air.local>
2026-03-29 20:28:53 +0530

    Config Updated

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config("./.env");
 
 export const config = {
+  CLIPDROP_API_KEY: process.env.CLIPDROP_API_KEY,
   ORIGIN: process.env.ORIGIN,
   PORT: process.env.PORT || 8000,
   MONGODB_URL: process.env.MONGODB_URL,
@@ -9,7 +10,6 @@ export const config = {
   ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
   REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
-  CLIPDROP_API_KEY: process.env.CLIPDROP_API_KEY,
   RAZORPAY_TEST_API_KEY: process.env.RAZORPAY_TEST_API_KEY,
   RAZORPAY_TEST_KEY_SECRET: process.env.RAZORPAY_TEST_KEY_SECRET,
   CURRENCY: process.env.CURRENCY,
